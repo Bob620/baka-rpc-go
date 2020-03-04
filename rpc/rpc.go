cbbf4a3eb1bcab6366d8a514c9ef5836149fc822
bob620 <bruder.kraft225@gmail.com>
2020-03-04 00:38:49 -0600

    feat(bakarpc) start func

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"baka-rpc-go/errors"
+	"baka-rpc-go/request"
 	"baka-rpc-go/response"
 	"bufio"
 	"encoding/json"
@@ -24,6 +25,7 @@ type method struct {
 }
 
 type MethodParam interface {
+	SetData(json.RawMessage) error
 	GetData() json.RawMessage
 }
 
@@ -63,11 +65,64 @@ func CreateBakaRpc(chanIn <-chan []byte, chanOut chan<- []byte) *bakaRpc {
 		chanOut = MakeWriterChan(os.Stdout)
 	}
 
-	return &bakaRpc{
+	rpc := &bakaRpc{
 		chanIn:  chanIn,
 		chanOut: chanOut,
 		methods: map[string]*method{},
 	}
+	rpc.start()
+
+	return rpc
+}
+
+func (rpc *bakaRpc) handleRequest(req request.Request) {
+
+}
+
+func (rpc *bakaRpc) handleResponse(res response.Response) {
+
+}
+
+func (rpc *bakaRpc) start() {
+	go func() {
+		res := response.Response{}
+		req := request.Request{}
+
+		for {
+			message := <-rpc.chanIn
+
+			err := json.Unmarshal(message, &req)
+			if err != nil {
+				err = json.Unmarshal(message, &res)
+				if err != nil {
+					data, _ := json.Marshal(response.NewErrorResponse("", errors.NewParseError()))
+					go rpc.sendMessage(data)
+				}
+			}
+
+			if req.GetRpcVersion() != "" {
+				if req.GetRpcVersion() != "2.0" {
+					data, _ := json.Marshal(response.NewErrorResponse(req.GetId(), errors.NewInvalidRequest()))
+					go rpc.sendMessage(data)
+				} else {
+					go rpc.handleRequest(req)
+				}
+			}
+
+			if res.GetRpcVersion() != "" {
+				if res.GetRpcVersion() != "2.0" {
+					data, _ := json.Marshal(response.NewErrorResponse(res.GetId(), errors.NewInvalidRequest()))
+					go rpc.sendMessage(data)
+				} else {
+					go rpc.handleResponse(res)
+				}
+			}
+		}
+	}()
+}
+
+func (rpc *bakaRpc) sendMessage(message json.RawMessage) {
+	rpc.chanOut <- message
 }
 
 func (rpc *bakaRpc) RegisterMethod(methodName string, methodParams []MethodParam, methodFunc MethodFunc) {
@@ -82,17 +137,17 @@ func (rpc *bakaRpc) DeRegisterMethod(methodName string) {
 	delete(rpc.methods, methodName)
 }
 
-func (rpc *bakaRpc) CallMethod(methodName string, methodParams map[string]MethodParam) *response.Response {
+func (rpc *bakaRpc) CallLocalMethod(methodName string, methodParams map[string]MethodParam) (success *response.Response, error *response.Response) {
 	method := rpc.methods[methodName]
 	if method == nil {
-		return response.NewErrorResponse("", errors.NewMethodNotFound())
+		return nil, response.NewErrorResponse("", errors.NewMethodNotFound())
 	}
 
 	returnMessage, err := (*method.methodFunc)(methodParams)
 
 	if err != nil {
-		return response.NewErrorResponse("", errors.NewGenericError(err.Error()))
+		return nil, response.NewErrorResponse("", errors.NewGenericError(err.Error()))
 	}
 
-	return response.NewSuccessResponse("", returnMessage)
+	return response.NewSuccessResponse("", returnMessage), nil
 }
